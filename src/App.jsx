55b3fcd113881f51d4df4e5c76086682ac2602b5
pushpender-79 <pushpenderchauhan79@gmail.com>
2026-03-13 18:26:42 +0530

    feat: setup React + Vite project with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-purple-600">
+        Celebrare Photo Gallery 
+      </h1>
+    </div>
+  );
+}
+
+export default App;
